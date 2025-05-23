bc780a2e5a5446842e344c72bb7496bed25dfa8e
kmdtaufik <kmdtaufik@noreplygihub.com>
2025-05-23 20:58:02 +0600

    feat: added webstorm

--- modules/devsuite.nix
+++ modules/devsuite.nix
@@ -1,12 +1,15 @@
-{ lib, config, ... }:
-
 {
+  lib,
+  config,
+  ...
+}: {
   imports = [
     ./vscode.nix
     ./idea.nix
+    ./webstorm.nix
   ];
 
   options.programs.devsuite.enable = lib.mkEnableOption "Enable the DevSuite bundle";
 
-  config = lib.mkIf config.programs.devsuite.enable { };
+  config = lib.mkIf config.programs.devsuite.enable {};
 }
